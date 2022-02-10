bffcef8877b27042d238b7f4f4803b9f4f2c08e8
Johnathan <sewelljohnathan@outlook.com>
2022-02-10 18:35:08 -0500

    Working version.

--- vm.c
+++ vm.c
@@ -43,12 +43,14 @@ void execute_program(instruction *code, int printFlag)
     int SP = BP - 1;
     int PC = 0;
     int RP = MAX_REG_LENGTH;
-    instruction IR;
+    
     int register_stack[MAX_REG_LENGTH];
     int data_stack[MAX_DATA_LENGTH];
-    int halt = 0;
+    instruction IR;
 
     char* opname = malloc(4*sizeof(char));
+    int halt = 0;
+    int line;
 
     for (int i = 0; i < MAX_DATA_LENGTH; i++) {
         data_stack[i] = 0;
@@ -62,10 +64,12 @@ void execute_program(instruction *code, int printFlag)
 		printf("\t\t\t\tPC\tBP\tSP\tRP\n");
 		printf("Initial values:\t\t\t%d\t%d\t%d\t%d\n", PC, BP, SP, RP);
 	}
-	
-    int line = 0;
+
     while (!halt) {
+
+        line = PC;
         IR = code[PC++];
+
         switch (IR.opcode) {
             case 1:
                 strcpy(opname, "LIT");
@@ -74,6 +78,9 @@ void execute_program(instruction *code, int printFlag)
 
                 if (IR.m == 0) {
                     strcpy(opname, "RET");
+                    SP = BP-1;
+                    PC = data_stack[SP+3];
+                    BP = data_stack[SP+2];
                     break;
                 }
 
@@ -108,7 +115,7 @@ void execute_program(instruction *code, int printFlag)
                         strcpy(opname, "LEQ");
                         temp = register_stack[RP+1] <= register_stack[RP++];
                     break; case 10:
-                        strcpy(opname, "GRT");
+                        strcpy(opname, "GTR");
                         temp = register_stack[RP+1] > register_stack[RP++];
                     break; case 11:
                         strcpy(opname, "GEQ");
@@ -116,46 +123,65 @@ void execute_program(instruction *code, int printFlag)
                     break;
                 }
                 register_stack[RP] = temp;
+
             break; case 3:
                 strcpy(opname, "LOD");
                 register_stack[--RP] = data_stack[base(IR.l, BP, data_stack) + IR.m];
+
             break; case 4:
                 strcpy(opname, "STO");
                 data_stack[base(IR.l, BP, data_stack) + IR.m] = register_stack[RP++];
+
             break; case 5:
                 strcpy(opname, "CAL");
+                int SL = base(IR.l, BP, data_stack);
+                int DL = BP;
+                int RA = PC;
+
+                data_stack[SP+1] = SL;
+                data_stack[SP+2] = DL;
+                data_stack[SP+3] = RA;
+
+                BP = SP+1;
+                PC = IR.m;
+
             break; case 6:
                 strcpy(opname, "INC");
                 SP += IR.m;
+
             break; case 7:
                 strcpy(opname, "JMP");
                 PC = IR.m;
+
             break; case 8:
                 strcpy(opname, "JPC");
                 if (register_stack[RP++] == 0) {
                     PC = IR.m;
                 }
+
             break; case 9: 
+
                 switch(IR.m) {
                     case 1:
                         strcpy(opname, "WRT");
                         printf("\t\tTop of Stack Value: %d\n", register_stack[RP++]);
+
                     break; case 2:
                         strcpy(opname, "RED");
                         printf("Please Enter an Integer:");
-                        int temp;
-                        scanf("%d", &temp);
+                        scanf("%d", &register_stack[--RP]);
                         printf("\n");
-                        register_stack[--RP] = temp;
+
                     break; case 3:
                         strcpy(opname, "HAL");
                         halt = 1;
+
                     break;
                 }
-            break;;
-        }
-        print_execution(line++, opname, IR, PC, BP, SP, RP, data_stack, register_stack);
-    }
 
- 
+            break;
+        }
+        
+        print_execution(line, opname, IR, PC, BP, SP, RP, data_stack, register_stack);
+    }
 }
